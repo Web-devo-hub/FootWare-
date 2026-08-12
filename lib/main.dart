6607a467fb7c9d3b48c4cff223cb907fbf85d921
kaifrrrr <kaifrao123@icloud.com>
2026-08-12 22:42:45 +0500

    - initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:footware/Authentication/loginpage.dart';
+import 'package:footware/Authentication/signup.dart';
+import 'package:footware/homepage.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => LogInPage(),
+        "Signup": (context) => SignUp(),
+        "homepage": (context) => HomePage(),
+      },
+    );
+  }
+}
+
